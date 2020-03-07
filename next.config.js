cc77f4ee1349ad480ab279c8ae7c132f47f6f55d
Håkan Fahlstedt <hf@fahlsoft.se>
2020-03-07 21:43:18 +0100

    Added MD content

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-  /* config options here */
-})
+const withPlugins = require('next-compose-plugins');
+const withSass = require('@zeit/next-sass');
+const withMDX = require('@next/mdx')({
+    extension: /\.mdx?$/,
+});
+
+module.exports = withPlugins([withSass, withMDX], {
+    pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+});
